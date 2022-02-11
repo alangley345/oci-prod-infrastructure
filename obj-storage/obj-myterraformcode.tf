30827dc8476659cf02bb326058307df3029cb48c
Aaron <alangley345@gmail.com>
2022-02-11 11:38:37 -0500

    Added folders for buckets and added state bucket definition

--- obj-storage/obj-myterraformcode.tf
+++ obj-storage/obj-myterraformcode.tf
@@ -5,6 +5,5 @@ resource "oci_objectstorage_bucket" "myterraformcode" {
     namespace = axplcpt6nqjo
 
     #Optional
-    access_type = NoPublicAccess
     freeform_tags = {"environment"= "prod"}
 }
