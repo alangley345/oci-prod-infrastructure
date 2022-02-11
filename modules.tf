14c7e3c526ea1a40036543eab48c4ac38ffef648
Aaron <alangley345@gmail.com>
2022-02-11 15:03:56 -0500

    Sorting out variables and dependencies

--- modules.tf
+++ modules.tf
@@ -2,12 +2,10 @@
 module "obj" {
  source = "./obj-storage"
 
- compartment_id = var.default_compartment
 }
 
 #virtual cloud networks
 module "vcn" {
  source = "./vcn"
 
- compartment_id = var.default_compartment
 }
