46189ecd6b1f8912e46183b207896fdafef47258
Aaron <alangley345@gmail.com>
2022-02-11 11:45:03 -0500

    Updating bucket definition for import

--- obj-storage/obj-myterraformcode.tf
+++ obj-storage/obj-myterraformcode.tf
@@ -1,8 +1,8 @@
 resource "oci_objectstorage_bucket" "myterraformcode" {
     #Required"
-    compartment_id = "ocid1.tenancy.oc1..aaaaaaaa56nnp6xcveam62bavbhvjcktkumwbvqd6yewpqqeufruwukmxzlq"
-    name = myterraformcode
-    namespace = axplcpt6nqjo
+    compartment_id = "alangley345"
+    name = "myterraformcode"
+    namespace = "axplcpt6nqjo"
 
     #Optional
     freeform_tags = {"environment"= "prod"}
