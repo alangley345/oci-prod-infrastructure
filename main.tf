fe1c1efa2da2fda4fc885042bf64666ca65efc6d
Aaron <alangley345@gmail.com>
2022-02-11 10:10:20 -0500

    added s3 backend for oci

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ terraform {
     endpoint = "https://axplcpt6nqjo.compat.objectstorage.ca-montreal-1.oraclecloud.com"
     shared_credentials_file     = "~/.terraform/credentials"
     skip_region_validation      = true
-    skip_credentials_validation = true
     skip_metadata_api_check     = true
     force_path_style            = true
   }
