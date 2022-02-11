b7fc0ff89c70e10e45499f22085bfcdf5d816165
Aaron <alangley345@gmail.com>
2022-02-11 10:40:30 -0500

    Cleaing up main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,6 @@ terraform {
     endpoint = "https://axplcpt6nqjo.compat.objectstorage.ca-montreal-1.oraclecloud.com"    
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_get_ec2_platforms      = true
     skip_metadata_api_check     = true
     force_path_style            = true
   }
