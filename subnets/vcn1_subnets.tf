0740abadee89b55e423c8976815ee73fb4cb2f5e
Aaron <alangley345@gmail.com>
2022-02-15 11:41:16 -0500

    Added subnet for vcn1

--- subnets/vcn1_subnets.tf
+++ subnets/vcn1_subnets.tf
@@ -3,4 +3,8 @@ resource "oci_core_subnet" "vcn1_subnet1" {
     cidr_block = "10.40.1.0/24"
     compartment_id = var.default_compartment
     vcn_id = var.vcn1_id
+
+    display_name = "Control Servers"
+    freeform_tags = {"environment"= "prod"}
+    ipv6cidr_block = 2603:c023:4002:600::/64
 }
