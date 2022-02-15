9bca2d1f4800af69861726eb754928f3269aaab2
Aaron <alangley345@gmail.com>
2022-02-15 11:16:41 -0500

    Added more variables

--- igw/variables.tf
+++ igw/variables.tf
@@ -2,3 +2,13 @@ variable "vcn_id1" {
     type = string
     default = ""
 }
+
+variable "comparment_id" {
+    type = string
+    default = ""
+}
+
+variable "vcn_id1" {
+    type = string
+    default = ""
+}
