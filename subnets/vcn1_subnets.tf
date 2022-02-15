5a100ec9e3e551a2e8a81678c38eb7c231a76a70
Aaron <alangley345@gmail.com>
2022-02-15 11:33:11 -0500

    Added subnet for vcn1

--- subnets/vcn1_subnets.tf
+++ subnets/vcn1_subnets.tf
@@ -1,6 +1,6 @@
 resource "oci_core_subnet" "vcn1_subnet1" {
     #Required
-    cidr_block = 10.40.0.1/24
+    cidr_block = "10.40.0.1/24"
     compartment_id = var.default_compartment
     vcn_id = var.vcn1_id
 }
