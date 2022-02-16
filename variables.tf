a4e541bcc90e37f360eecf229dff417e1af56f15
Aaron Langley <alangley345@gmail.com>
2022-02-15 21:43:33 -0500

    updated the default_compartment value

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "default_compartment" {
  type = string
- default = "ocid1.tenancy.oc1..aaaaaaaa56nnp6xcveam62bavbhvjcktkumwbvqd6yewpqqeufruwukmxzlq"
+ default = "ocid1.vcn.ca-montreal-1.oc1..aaaaaaaa56nnp6xcveam62bavbhvjcktkumwbvqd6yewpqqeufruwukmxzlq"
 }
