b759d1f4c337594cd2179c4fc0d0ed094bec3448
Aaron Langley <alangley345@gmail.com>
2022-02-15 21:47:04 -0500

    updated the default_compartment value

--- subnets/vcn1_subnets.tf
+++ subnets/vcn1_subnets.tf
@@ -1,7 +1,7 @@
 resource "oci_core_subnet" "vcn1_subnet1" {
     #Required
     cidr_block = "10.40.1.0/24"
-    compartment_id = var.default_compartment
+    compartment_id = ocid1.vcn.oc1.ca-montreal-1.amaaaaaa4q67ctyahntpegh2aipdmj76rv44hajlnn6x4anbjdgicaghtifa
     vcn_id = var.vcn1_id
 
     display_name = "Control Servers"
